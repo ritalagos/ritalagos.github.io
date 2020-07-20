5efc5ce4232534a5add666b2db36a545a35be35a
GITCART <cartolin1011@gmail.com>
2020-07-19 22:56:11 -0500

    Portafolio

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/***Loader***/
+/*
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
+*/
+
+/*
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})*/
